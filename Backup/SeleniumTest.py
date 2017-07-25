916eb703faa5738da1198728cd3fa919f12f16d6
Masonic <Masonic9@live.com>
2017-07-25 14:59:23 +0800

    Initial commit

---
+++ Backup/SeleniumTest.py
@@ -0,0 +1,104 @@
+import os
+
+from selenium import webdriver
+
+
+def main():
+    global browser
+
+    url = "https://passport.baidu.com/v2/?login&tpl=mn&u=http%3A%2F%2Fwww.baidu.com%2F"
+    # 打开谷歌浏览器
+    # Firefox()
+    # Chrome()
+    chromedriver = 'E:\PyCharmProjects\Chrome\chromedriver'
+
+    browser = webdriver.Chrome(chromedriver)
+
+    os.environ["webdriver.chrome.driver"] = chromedriver
+
+    # 输入网址
+    browser.get(url)
+    # 打开浏览器时间
+    # print("等待10秒打开浏览器...")
+    # time.sleep(10)
+
+    # 找到id="TANGRAM__PSP_3__userName"的对话框
+    # 清空输入框
+    browser.find_element_by_id("TANGRAM__PSP_3__userName").clear()
+    browser.find_element_by_id("TANGRAM__PSP_3__password").clear()
+
+    # 输入账号密码
+    # 输入账号密码
+    account = []
+    try:
+        fileaccount = open("../baidu/account.txt")
+        accounts = fileaccount.readlines()
+        for acc in accounts:
+            account.append(acc.strip())
+        fileaccount.close()
+    except Exception as err:
+        print(err)
+        input("请正确在account.txt里面写入账号密码")
+        exit()
+    browser.find_element_by_id("TANGRAM__PSP_3__userName").send_keys(account[0])
+    browser.find_element_by_id("TANGRAM__PSP_3__password").send_keys(account[1])
+
+    # 点击登陆登陆
+    # id="TANGRAM__PSP_3__submit"
+    browser.find_element_by_id("TANGRAM__PSP_3__submit").click()
+
+    # 等待登陆10秒
+    # print('等待登陆10秒...')
+    # time.sleep(10)
+    print("等待网址加载完毕...")
+
+    select = input("请观察浏览器网站是否已经登陆(y/n)：")
+    while 1:
+        if select == "y" or select == "Y":
+            print("登陆成功！")
+            print("准备打开新的窗口...")
+            # time.sleep(1)
+            # browser.quit()
+            break
+
+        elif select == "n" or select == "N":
+            selectno = input("账号密码错误请按0，验证码出现请按1...")
+            # 账号密码错误则重新输入
+            if selectno == "0":
+
+                # 找到id="TANGRAM__PSP_3__userName"的对话框
+                # 清空输入框
+                browser.find_element_by_id("TANGRAM__PSP_3__userName").clear()
+                browser.find_element_by_id("TANGRAM__PSP_3__password").clear()
+
+                # 输入账号密码
+                account = []
+                try:
+                    fileaccount = open("../baidu/account.txt")
+                    accounts = fileaccount.readlines()
+                    for acc in accounts:
+                        account.append(acc.strip())
+                    fileaccount.close()
+                except Exception as err:
+                    print(err)
+                    input("请正确在account.txt里面写入账号密码")
+                    exit()
+
+                browser.find_element_by_id("TANGRAM__PSP_3__userName").send_keys(account[0])
+                browser.find_element_by_id("TANGRAM__PSP_3__password").send_keys(account[1])
+                # 点击登陆sign in
+                # id="TANGRAM__PSP_3__submit"
+                browser.find_element_by_id("TANGRAM__PSP_3__submit").click()
+
+            elif selectno == "1":
+                # 验证码的id为id="ap_captcha_guess"的对话框
+                input("请在浏览器中输入验证码并登陆...")
+                select = input("请观察浏览器网站是否已经登陆(y/n)：")
+
+        else:
+            print("请输入“y”或者“n”！")
+            select = input("请观察浏览器网站是否已经登陆(y/n)：")
+
+
+if __name__ == "__main__":
+    main()
---
+++ Weibo/WeiIndex.py
@@ -0,0 +1,77 @@
+# coding=utf-8
+import json
+import os
+from urllib import parse
+
+import requests
+import xlrd
+
+Rootdir = os.path.abspath(os.path.dirname(os.getcwd()))
+Exceldir = Rootdir + r"\Dataset\登峰杯播放社交全.xls"
+Sheet = "Sheet1"
+date_start = "2014-01-01"
+date_stop = "2017-07-09"
+
+
+def read_xls_file(directory, sheet):
+    xls = xlrd.open_workbook(directory)
+    s = xls.sheet_by_name(sheet)
+    cols = s.col_values(0)
+    return cols
+
+
+def savejson(soapname, data):
+    fl = open('../OutPut/14to17/' + soapname + '.json', 'w')
+    fl.write(json.dumps(data, ensure_ascii=False, indent=2))
+    fl.close()
+
+
+def search_name(name):
+    url_format = "http://data.weibo.com/index/ajax/hotword?word={}&flag=nolike&_t=0"
+    # 伪造cookie
+    cookie_header = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.81 Safari/537.36",
+        "Referer": "http://data.weibo.com/index?sudaref=www.google.com"
+    }
+
+    urlname = parse.quote(name)
+    first_requests = url_format.format(urlname)
+    codes = requests.get(first_requests, headers=cookie_header).json()
+    ids = codes["data"]["id"]
+    # 伪造包头
+    header = {
+        "Connection": "keep-alive",
+        "Accept-Encoding": "gzip, deflate, sdch",
+        "Accept": "*/*",
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.81 Safari/537.36",
+        "Accept-Language": "zh-CN,zh;q=0.8",
+        "Referer": "http://data.weibo.com/index/hotword?wid={}&wname={}".format(ids, urlname),
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Host": "data.weibo.com"
+    }
+
+    # 获取日期
+    date_url = "http://data.weibo.com/index/ajax/getdate?month=1&__rnd=1498190033389"
+    dc = requests.get(date_url, headers=header).json()
+    edate, sdate = dc["edate"], dc["sdate"]
+
+    # 数据返回
+    codes = requests.get("http://data.weibo.com/index/ajax/getchartdata?wid={}&sdate="+date_start+"&edate="+date_stop \
+                         .format(ids, sdate, edate), headers=header).json()
+    return codes
+
+
+if __name__ == "__main__":
+    # 自动爬取新浪微博微指数，结果保存于Output/14to17
+    dataless = ["女管家", "飘洋过海来看你", "守护丽人"]
+    # 以上为未收录数据
+    for soap in read_xls_file(Exceldir, Sheet):
+        if os.path.exists('../OutPut/14to17/' + soap + '.json'):
+            print(soap + "已保存过")
+            continue
+        if soap == "" or dataless.__contains__(soap):
+            # 爱情公寓四未收录，以爱情公寓为关键词查询
+            continue
+        savejson(soap, search_name(soap))
+        print(soap + "已保存")
+    print("已完成")
