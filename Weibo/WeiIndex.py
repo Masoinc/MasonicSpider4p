eb06151f802ea3300a58d7b7a8397a3b3954964e
Masonic <Masonic9@live.com>
2017-09-10 11:30:51 +0800

    MasonicSpider4p

--- Weibo/WeiIndex.py
+++ Weibo/WeiIndex.py
@@ -28,22 +28,31 @@ def savejson(soapname, data):
 
 def search_name(name):
     url_format = "http://data.weibo.com/index/ajax/hotword?word={}&flag=nolike&_t=0"
+
     # 伪造cookie
     cookie_header = {
         "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.81 Safari/537.36",
         "Referer": "http://data.weibo.com/index?sudaref=www.google.com"
     }
-
+    # 汉字转为%编码
     urlname = parse.quote(name)
+    # 将{}替换为关键词
     first_requests = url_format.format(urlname)
+    print(first_requests)
+
     codes = requests.get(first_requests, headers=cookie_header).json()
+
+    print(codes)
+
+    # 获取关键词代码
     ids = codes["data"]["id"]
-    # 伪造包头
+
+    # 伪造完整包头
     header = {
         "Connection": "keep-alive",
         "Accept-Encoding": "gzip, deflate, sdch",
         "Accept": "*/*",
-        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.81 Safari/537.36",
+        "User-Agent": "ksoap2-android/2.6.0+",
         "Accept-Language": "zh-CN,zh;q=0.8",
         "Referer": "http://data.weibo.com/index/hotword?wid={}&wname={}".format(ids, urlname),
         "Content-Type": "application/x-www-form-urlencoded",
@@ -54,10 +63,28 @@ def search_name(name):
     date_url = "http://data.weibo.com/index/ajax/getdate?month=1&__rnd=1498190033389"
     dc = requests.get(date_url, headers=header).json()
     edate, sdate = dc["edate"], dc["sdate"]
-
+    print(dc)
     # 数据返回
+    # 指定月份指数数据
+    # getchartdata?month=3&_rnd=时间戳
+    # ?month为月份跨度
+
+    # 日期数据
+    # getdate?month=3&_rnd=时间戳
+    # ?month为月份跨度
+    print(codes)
     codes = requests.get("http://data.weibo.com/index/ajax/getchartdata?wid={}&sdate=2014-01-01&edate=2017-07-29".format(ids, sdate, edate), headers=header).json()
+
     return codes
+
+    # 指定日期区间获取微指数数据url
+    # http://data.weibo.com/index/ajax/getchartdata?wid=1061704100000146164&sdate=2017-09-01&edate=2017-09-07&__rnd=1504856390847
+    # sdate起始日期
+    # edate截止日期
+    # 伪造包头Referer格式
+    # http://data.weibo.com/index/hotword?wid=1061704100000146164&wname=%E4%BA%BA%E6%B0%91%E7%9A%84%E5%90%8D%E4%B9%89
+    # &wid为关键词代码
+    # &wname为%格式化后的关键词
 
 
 if __name__ == "__main__":
@@ -74,13 +101,13 @@ if __name__ == "__main__":
         savejson(soap, search_name(soap))
         print(soap + "已保存")
     print("已完成")
-    # for soap in read_xls_file(Exceldir, Sheet):
-    #     if os.path.exists('../OutPut/14to17/' + soap + '.json'):
-    #         print(soap + "已保存过")
-    #         continue
-    #     if soap == "" or dataless.__contains__(soap):
-    #         # 爱情公寓四未收录，以爱情公寓为关键词查询
-    #         continue
-    #     savejson(soap, search_name(soap))
-    #     print(soap + "已保存")
-    # print("已完成")
+    for soap in read_xls_file(Exceldir, Sheet):
+        if os.path.exists('../OutPut/14to17/' + soap + '.json'):
+            print(soap + "已保存过")
+            continue
+        if soap == "" or dataless.__contains__(soap):
+            # 爱情公寓四未收录，以爱情公寓为关键词查询
+            continue
+        savejson(soap, search_name(soap))
+        print(soap + "已保存")
+    print("已完成")
